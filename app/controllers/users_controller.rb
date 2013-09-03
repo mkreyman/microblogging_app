42fac77fee0114b64dc9a49fc10d7c5b8de9726a
Mark Kreyman <mark@kreyman.com>
2013-09-02 18:39:14 -0600

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :signed_in_but_trying_to_sign_up_again, only: [:new, :create]  # 9.6 Exercises, ex.6
@@ -59,6 +59,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -100,6 +100,16 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
 
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
       end  # End of "in the Users controller" block
 
       describe "in the Microposts controller" do
@@ -114,6 +124,22 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end # end of "in the Microposts controller" block
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        # Note that, in order to avoid the overhead of creating a virtually useless Relationship object, 
+        # the delete test hard-codes the id 1 in the named route:
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end  # "in the Relationships controller" block
+
     end  # End of "for non-signed-in users" block
 
     describe "as wrong user" do
